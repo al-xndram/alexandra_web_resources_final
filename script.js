0cbd11f0989b86c8c154f2a674e705a2fce1d99c
alisq <ali.shamas.qadeer@gmail.com>
2024-03-07 15:25:39 -0500

    made a bunch of changes

--- script.js
+++ script.js
@@ -1,57 +1,60 @@
-// sound functions
-
-let modal_open = false;
-
-// Function to play individual sound when hovered
-
-function playSound(soundId) {
-  if (modal_open === true) return;
-  // Pause all sounds
-  const sounds = document.getElementsByTagName("audio");
-  for (let i = 0; i < sounds.length; i++) {
-    sounds[i].pause();
-    sounds[i].currentTime = 0;
-  }
-  // Play the selected sound
-  const sound = document.getElementById(soundId);
-  sound.play();
-}
-
-// Function to play all sounds initially
-window.onload = function () {
-  const sounds = document.getElementsByTagName("audio");
-  for (let i = 0; i < sounds.length; i++) {
-    sounds[i].play();
-  }
-};
-
-// Function to stop all sounds when mouse leaves sound element
-function stopSound() {
-  if (modal_open === true) return;
-
-  const sounds = document.getElementsByTagName("audio");
-  for (let i = 0; i < sounds.length; i++) {
-    sounds[i].pause();
-    sounds[i].currentTime = 0;
-  }
-  // Play all sounds again
-  window.onload();
-}
-
-//MODAL (popup windows)
-
-// Function to open the modal and play corresponding audio
-function openModal(text, audioId) {
-  let modal = document.querySelector("#" + text);
-  modal.style.display = "block";
-  modal_open = true;
-}
-
-// Function to close the modal
-function closeModal(text) {
-  modal_open = false;
-
-  console.log("ok");
-  let modal = document.querySelector("#" + text);
-  modal.style.display = "none";
-}
+$(document).ready(function () {
+    $("#abstract").hover(
+      function () {
+        $(".abstract_index").css("color", "white");
+      },
+      function () {
+        $(".abstract_index").css("color", "#2f0f6a");
+      }
+    );
+  
+    $(".abstract_index").click(function (e) {
+      e.preventDefault();
+      $(this).toggleClass("active");
+      $(".content").toggleClass("contentActive");
+    });
+  
+  
+    $(".sound_index, .abstract_index").hover(function() {
+      $("audio").prop("muted", true);
+      $(this).find("audio").prop("muted", false);
+      
+    },
+    function() {
+      $("audio").prop("muted", false);
+      $(".content").removeClass("contentActive");
+  
+    });
+  
+  
+  
+  $(".sound_index").click(function(){
+    
+    $(this).find(".modal").addClass("active")
+  })
+  
+  $(".close").click(function(){
+    
+    setTimeout(function(){
+      $(".modal").removeClass("active");
+    },50)
+  })
+  
+  
+  $(".abstract_index").click(function() {
+    $("audio").prop("muted", true);
+    
+    a = document.getElementById($(this).find("audio").attr("id"));
+  
+    $(this).find("audio")
+      .prop("muted", false);
+  
+  
+  
+  
+  });
+  
+  
+  
+  });
+  
