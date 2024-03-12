3107c837f010e66749541c410cc7d04980b3c7a2
al-xndram <157516626+al-xndram@users.noreply.github.com>
2024-03-12 00:23:55 -0400

    ali merge (#2)
    * made a bunch of changes
    
    * folder structure
    
    ---------
    
    Co-authored-by: alisq <ali.shamas.qadeer@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+    $("#abstract").hover(
+      function () {
+        $(".abstract_index").css("color", "white");
+      },
+      function () {
+        $(".abstract_index").css("color", "#2f0f6a");
+      }
+    );
+  
+    $(".abstract_index").click(function (e) {
+      e.preventDefault();
+      $(this).toggleClass("active");
+      $(".content").toggleClass("contentActive");
+    });
+  
+  
+    $(".sound_index, .abstract_index").hover(function() {
+      $("audio").prop("muted", true);
+      $(this).find("audio").prop("muted", false);
+      
+    },
+    function() {
+      $("audio").prop("muted", false);
+      $(".content").removeClass("contentActive");
+  
+    });
+  
+  
+  
+  $(".sound_index").click(function(){
+    
+    $(this).find(".modal").addClass("active")
+  })
+  
+  $(".close").click(function(){
+    
+    setTimeout(function(){
+      $(".modal").removeClass("active");
+    },50)
+  })
+  
+  
+  $(".abstract_index").click(function() {
+    $("audio").prop("muted", true);
+    
+    a = document.getElementById($(this).find("audio").attr("id"));
+  
+    $(this).find("audio")
+      .prop("muted", false);
+  
+  
+  
+  
+  });
+  
+  
+  
+  });
+  
